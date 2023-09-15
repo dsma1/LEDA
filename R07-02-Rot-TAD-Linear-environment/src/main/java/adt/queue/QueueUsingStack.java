0b96f70ee276ba8373f75c6ebc05fd2e7dc0a225
Diego Aleixo <dsoutomaiora@gmail.com>
2023-09-15 11:11:17 -0300

    R07 fim

--- R07-02-Rot-TAD-Linear-environment/src/main/java/adt/queue/QueueUsingStack.java
+++ R07-02-Rot-TAD-Linear-environment/src/main/java/adt/queue/QueueUsingStack.java
@@ -2,6 +2,8 @@ package adt.queue;
 
 import adt.stack.Stack;
 import adt.stack.StackImpl;
+import adt.stack.StackOverflowException;
+import adt.stack.StackUnderflowException;
 
 public class QueueUsingStack<T> implements Queue<T> {
 
@@ -15,32 +17,55 @@ public class QueueUsingStack<T> implements Queue<T> {
 
 	@Override
 	public void enqueue(T element) throws QueueOverflowException {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (!stack2.isEmpty()) {
+			swapStackValues(stack2, stack1);
+		}
+		
+		try {
+			stack1.push(element);
+		} catch (StackOverflowException e)  {
+			throw new QueueOverflowException();
+		}
 	}
 
 	@Override
 	public T dequeue() throws QueueUnderflowException {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		swapStackValues(stack1, stack2);
+		T result = stack2.top();
+
+		try {
+			stack2.pop();
+		} catch (StackUnderflowException e) {
+			throw new QueueUnderflowException();
+		}
+
+		return result;
 	}
 
 	@Override
 	public T head() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		swapStackValues(stack1, stack2);
+		T result = stack2.top();
+
+		return result;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		return stack1.isEmpty() && stack2.isEmpty();
 	}
 
 	@Override
 	public boolean isFull() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		return stack1.isFull() || stack2.isFull();
 	}
-
+	
+	private void swapStackValues(Stack<T> firstStack, Stack<T> secondStack) {
+		while(!firstStack.isEmpty()) {
+			try {
+				secondStack.push(firstStack.pop());
+			} catch (StackUnderflowException | StackOverflowException e) {}
+		}
+	}
+	
 }
